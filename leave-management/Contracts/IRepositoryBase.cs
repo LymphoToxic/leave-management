ad800a3eeb3beba73d44c6ee4cab965e453b3c43
Rutger Wechgelaar <Rutger.Wechgelaar@awisoftware.nl>
2020-08-20 11:39:54 +0200

    Add project files.

---
+++ leave-management/Contracts/IRepositoryBase.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace leave_management.Contracts
+{
+    public interface IRepositoryBase<T> where T : class
+    {
+        ICollection<T> FindAll();
+        T FindbyId(int id);
+        bool Create(T Entity);
+        bool Update(T Entity);
+        bool Delete(T Entity);
+        bool Save();
+    }
+}
